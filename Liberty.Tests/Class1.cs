e2670d6d475735648ed084290680178b3d693040
davidwr6 <bania.david@gmail.com>
2015-07-10 11:17:13 +0100

    Added nunit framework and created first test

---
+++ Liberty.Tests/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Liberty.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [SetUp]
+        public void Setup()
+        { 
+            //do stuff
+        }
+
+        [Test]
+        public void Should_Do_Something()
+        {
+            Func<int, bool> isMultipleOf2 = x => x % 2 == 0;
+
+            int numb1 = 4;
+            int numb2 = 23;
+
+            Assert.That(isMultipleOf2(numb1), Is.True);
+            Assert.That(isMultipleOf2(numb2), Is.False);
+        }
+    }
+}
